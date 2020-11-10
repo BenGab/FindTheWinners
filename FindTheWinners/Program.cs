51a88e7671a459cac40aa6469539dcb421db02fd
Gabor Benko <benkogabor@msn.com>
2020-11-10 17:56:30 +0100

    working soltuion

--- FindTheWinners/Program.cs
+++ FindTheWinners/Program.cs
@@ -7,39 +7,8 @@ namespace FindTheWinners
     {
         static void Main(string[] args)
         {
-            List<TestData> testDatas1 = new List<TestData>()
-            {
-                new TestData(0, 1.00)
-            };
-            TestCase testcase1 = new TestCase(1,1, 1, 1, testDatas1);
-            testcase1.Run();
-
-            //0 0.10
-            //0 0.89
-            //1 0.99
-            List<TestData> testDatas2 = new List<TestData>()
-            {
-                new TestData(0, 0.10),
-                new TestData(0, 0.89),
-                new TestData(1, 0.99)
-            };
-
-            TestCase testCase2 = new TestCase(2, 3, 2, 1, testDatas2);
-            testCase2.Run();
-
-//            1 320.80
-//0 69.20
-//1 564.50
-            List<TestData> testDatas3 = new List<TestData>()
-            {
-                new TestData(0, 320.80),
-                new TestData(0, 69.20),
-                new TestData(1, 564.50)
-            };
-
-            TestCase testCase3 = new TestCase(3, 3, 2, 10, testDatas3);
-            testCase3.Run();
-
+            TestCaseLoader loader = new TestCaseLoader(Convert.ToInt32(Console.ReadLine().Trim()));
+            loader.GenerateTestCases().ForEach(x=> x.Run());
         }
     }
 }
---
+++ FindTheWinners/TestCaseLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FindTheWinners
+{
+    public class TestCaseLoader
+    {
+        private readonly int numberofTestCases;
+
+        public TestCaseLoader(int numberofTestCases)
+        {
+            this.numberofTestCases = numberofTestCases;
+        }
+
+        public List<TestCase> GenerateTestCases()
+        {
+            var result = new List<TestCase>();
+
+            for (int i = 1; i <= numberofTestCases; i++)
+            {
+                string[] testCaseData = Console.ReadLine().Trim().Split(' ');
+                List<TestData> testDatas = new List<TestData>();
+
+                for (int j = 0; j < Convert.ToInt32(testCaseData[0]); j++)
+                {
+                    string[] testData = Console.ReadLine().Trim().Split(' ');
+                    testDatas.Add(new TestData(Convert.ToInt32(testData[0]), double.Parse(testData[1], CultureInfo.InvariantCulture)));
+                }
+
+                result.Add(new TestCase(i, Convert.ToInt32(testCaseData[0]), Convert.ToInt32(testCaseData[1]), double.Parse(testCaseData[2], CultureInfo.InvariantCulture), testDatas));
+            }
+
+            return result;
+        }
+    }
+}
